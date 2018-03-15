c893e3b875f88ff4327a9208557bbfd90d06c2a2
Mark Freeman <mfreema1@stevens.edu>
2018-03-14 21:49:45 -0400

    Finish hw4

--- HCScode/hw4/problem_1.asm
+++ HCScode/hw4/problem_1.asm
@@ -1,12 +1,13 @@
+lwcs:	EQU	$60
 	ORG	$5000
 	dc.b	"Mark Freeman 2020"
 	ORG	$4000
 	ldx	#$5000
 loop:	ldaa	0,X
 	tbeq	A, done
-	cmpa	#$60
-	blo	uprcs
+	cmpa	lwcs
+	blo	upcs
 	suba	#$20
-uprcs:	staa	1, X+
+upcs:	staa	1, X+
 	bra	loop
 done:	swi
